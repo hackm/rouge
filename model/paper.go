98c5147ee44f935d36d8a121ff0247dd17cf8d31
Orleika <orleika@users.noreply.github.com>
2016-05-04 23:47:04 +0000

    Fixes the relation between paper and tag

--- model/paper.go
+++ model/paper.go
@@ -9,7 +9,7 @@ type Paper struct {
 	ID        uint32 `json:"id" form:"id" db:"id"`
 	User      *User
 	Content   *Content
-	Tag       *Tag
+	Tags      []*Tag
 	CreatedAt time.Time `json:"createdAt" form:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" form:"updatedAt" db:"updated_at"`
 }
--- model/tag.go
+++ model/tag.go
@@ -2,7 +2,7 @@ package model
 
 // Tag struct
 type Tag struct {
-	ID    uint32 `json:"id" form:"id" db:"id"`
-	Name  string `json:"name" form:"name" db:"name"`
-	Paper *Paper
+	ID     uint32 `json:"id" form:"id" db:"id"`
+	Name   string `json:"name" form:"name" db:"name"`
+	Papers []*Paper
 }
