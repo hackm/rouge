3a168770457d45f34fe08a1fa04d429940fac773
Shun Hosaka <reyurnible@gmail.com>
2016-05-05 21:09:39 +0900

    fix import name

--- routes/router.go
+++ routes/router.go
@@ -3,11 +3,11 @@ package route
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"../controllers/api/content"
+	apiContent "../controllers/api/content"
 	apiPaper "../controllers/api/paper"
 	apiTag "../controllers/api/tag"
-	"../controllers/api/stock"
-	"../controllers/api/user"
+	apiStock "../controllers/api/stock"
+	apiUser "../controllers/api/user"
 	"../controllers/top"
 	"../controllers/paper"
 	"../controllers/tag"
@@ -26,7 +26,7 @@ func Init() *echo.Echo {
 	api := e.Group("/api")
 	{
 		// User
-		api.Post("users", user.CreateUser)
+		api.Post("users", apiUser.CreateUser)
 
 		//User Papers
 		api.Get("/papers", apiPaper.GetUserPapers)
@@ -36,16 +36,16 @@ func Init() *echo.Echo {
 		api.Delete("/papers/:id", apiPaper.DeletePaper)
 
 		//User Paper Conntents
-		api.Get("/papers/:id/contents", content.GetContents)
+		api.Get("/papers/:id/contents", apiContent.GetContents)
 
 		//User Paper Tags
 		api.Post("/papers/:id/tags", apiPaper.AddPaperTag)
 		api.Delete("/papers/:id/tags", apiPaper.DeletePaperTag)
 
 		// User Stocks
-		api.Get("/stocks", stock.GetStocks)
-		api.Post("/stocks/:paper_id", stock.CreateStock)
-		api.Delete("/stocks/:paper_id", stock.DeleteStock)
+		api.Get("/stocks", apiStock.GetStocks)
+		api.Post("/stocks/:paper_id", apiStock.CreateStock)
+		api.Delete("/stocks/:paper_id", apiStock.DeleteStock)
 
 		// Tag
 		api.Get("/tags", apiTag.GetTags)
