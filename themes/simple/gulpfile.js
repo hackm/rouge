7176c06cd4c2aca7ff6ca30dab2b7a8adf8f9e06
iwate <iwatesoftware@live.jp>
2016-05-05 12:24:02 +0900

    create simple theme

---
+++ themes/simple/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var riot = require('gulp-riot');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('riot', function() {
+    return gulp.src('src/**/*.tag.html')
+        .pipe(riot())
+        .pipe(gulp.dest('build'));
+});
+gulp.task('compress',['riot'], function () {
+    return gulp.src('build/**/*.js')
+        .pipe(uglify())
+        .pipe(concat('tags.js'))
+        .pipe(gulp.dest('../../static/themes/simple'));
+});
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.tag.html', ['compress']);
+})
+gulp.task('default', ['compress']);
