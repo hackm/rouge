314dfd41f1f99b3465600935d7e6a0d6bdcf5016
Shun Hosaka <reyurnible@gmail.com>
2016-05-05 22:53:27 +0900

    fix error

--- models/paper_service/paper_service.go
+++ models/paper_service/paper_service.go
@@ -1,75 +1,80 @@
 package paper_service
 
-import(
-  "../repositories/user"  
-  "../repositories/paper"
-  "../repositories/content"
+import (
+	"../repositories/user"
+	"../repositories/paper"
+	"../repositories/content"
 )
 
 type PaperService struct {
-  UserRepository *user.UserRepository  
-  PaperRepository *paper.PaperRepository
-  ContentRepository *content.ContentRepository
+	UserRepository    user.IUserRepository
+	PaperRepository   paper.IPaperRepository
+	ContentRepository content.IContentRepository
 }
 
-func NewPaperService() *PaperService{
-  return &PaperService{
-    UserRepository: NewUserRepository(),    
-    PaperRepository: NewPaperRepository(),
-    ContentRepository: NewContentRepository(),
-  }
+func NewPaperService() *PaperService {
+	return &PaperService{
+		UserRepository: user.NewUserRepository(),
+		PaperRepository: paper.NewPaperRepository(),
+		ContentRepository: content.NewContentRepository(),
+	}
 }
 
 func (ps *PaperService) GetPaper(id int64) paper.Paper {
-  p := ps.PaperRepository.GetPaper(id);
-  _, cc := p["content"]
-  _, uc := p["user"]
-  if !cc
-    p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
-  if !uc
-    p["user"] = ps.NewUserRepository.GetUser(p["userId"]);
-  
-  return p
+	p := ps.PaperRepository.GetPaper(id)
+	_, cc := p["content"]
+	_, uc := p["user"]
+	if !cc {
+		p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
+	}
+	if !uc {
+		p["user"] = ps.UserRepository.GetUser(p["userId"]);
+	}
+	return p
 }
 
 func (ps *PaperService) GetUserPapers(id int64) paper.Paper {
-  pl := ps.PaperRepository.GetUserPapers(id);
-  for p := range pl {
-    _, cc := p["content"]
-    _, uc := p["user"]
-    if !cc
-      p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
-    if !uc
-      p["user"] = ps.NewUserRepository.GetUser(p["userId"]);
-  }
-  
-  return pl
+	pl := ps.PaperRepository.GetUserPapers(id);
+	for p := range pl {
+		_, cc := p["content"]
+		_, uc := p["user"]
+		if !cc {
+			p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
+		}
+		if !uc {
+			p["user"] = ps.UserRepository.GetUser(p["userId"]);
+		}
+	}
+
+	return pl
 }
 
 func (ps *PaperService) GetTagPapers(tag string) paper.Paper {
-  pl := ps.PaperRepository.GetTagPapers(tag);
-  for p := range pl {
-    _, cc := p["content"]
-    _, uc := p["user"]
-    if !cc
-      p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
-    if !uc
-      p["user"] = ps.NewUserRepository.GetUser(p["userId"]);
-  }
-  
-  return pl
+	pl := ps.PaperRepository.GetTagPapers(tag);
+	for p := range pl {
+		_, cc := p["content"]
+		_, uc := p["user"]
+		if !cc {
+			p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
+		}
+		if !uc {
+			p["user"] = ps.UserRepository.GetUser(p["userId"]);
+		}
+	}
+	return pl
 }
 
 func (ps *PaperService) GetPapers(keyword string) paper.Paper {
-  pl := ps.PaperRepository.GetPapers(keyword);
-  for p := range pl {
-    _, cc := p["content"]
-    _, uc := p["user"]
-    if !cc
-      p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
-    if !uc
-      p["user"] = ps.NewUserRepository.GetUser(p["userId"]);
-  }
-  
-  return pl
+	pl := ps.PaperRepository.GetPapers(keyword);
+	for p := range pl {
+		_, cc := p["content"]
+		_, uc := p["user"]
+		if !cc {
+			p["content"] = ps.ContentRepository.GetContent(p["contentId"]);
+		}
+		if !uc {
+			p["user"] = ps.UserRepository.GetUser(p["userId"]);
+		}
+	}
+	return pl
 }
--- models/repositories/paper/paper.go
+++ models/repositories/paper/paper.go
@@ -3,68 +3,68 @@ package paper
 type Paper map[string]interface{}
 
 type IPaperRepository interface {
-  GetPaper(id int64) Paper
-  GetUserPapers(userId int64) []Paper
-  GetTagPapaers(tag string) []Paper
-  GetPapaers(keyword string) []Paper
-  
-  CreatePaper(p Paper) error
-  UpdatePaper(p Paper) error
-  DeletePaper(id int64) error
+	GetPaper(id int64) Paper
+	GetUserPapers(userId int64) []Paper
+	GetTagPapers(tag string) []Paper
+	GetPapers(keyword string) []Paper
+
+	CreatePaper(p Paper) error
+	UpdatePaper(p Paper) error
+	DeletePaper(id int64) error
 }
 
-type PaperRepository struct {}
+type PaperRepository struct{}
 
-func NewPaperRepository *PaperRepository{
-  return &PaperRepository{}
+func NewPaperRepository() *PaperRepository {
+	return &PaperRepository{}
 }
 
 func (r *PaperRepository) GetPaper(id int64) Paper {
-  p := make(Paper)
-  p["id"] = id
-  p["userId"] = 1000
-  p["contentId"] = 2000
-  
-  return p
+	p := make(Paper)
+	p["id"] = id
+	p["userId"] = 1000
+	p["contentId"] = 2000
+
+	return p
 }
 func (r *PaperRepository) GetUserPapers(userId int64) []Paper {
-  p1 := make(Paper)
-  p1["id"] = 1000
-  p1["userId"] = 1000
-  p1["contentId"] = 2000
-  
-  p2 := make(Paper)
-  p2["id"] = 1001
-  p2["userId"] = 1000
-  p2["contentId"] = 2001
-  
-  return []Paper{p1, p2}
+	p1 := make(Paper)
+	p1["id"] = 1000
+	p1["userId"] = 1000
+	p1["contentId"] = 2000
+
+	p2 := make(Paper)
+	p2["id"] = 1001
+	p2["userId"] = 1000
+	p2["contentId"] = 2001
+
+	return []Paper{p1, p2}
 }
-func (r *PaperRepository) GetTagPapaers(tag string) []Paper {
-  p1 := make(Paper)
-  p1["id"] = 1000
-  p1["userId"] = 1000
-  p1["contentId"] = 2000
-  
-  p2 := make(Paper)
-  p2["id"] = 1001
-  p2["userId"] = 1000
-  p2["contentId"] = 2001
-  
-  return []Paper{p1, p2}
+func (r *PaperRepository) GetTagPapers(tag string) []Paper {
+	p1 := make(Paper)
+	p1["id"] = 1000
+	p1["userId"] = 1000
+	p1["contentId"] = 2000
+
+	p2 := make(Paper)
+	p2["id"] = 1001
+	p2["userId"] = 1000
+	p2["contentId"] = 2001
+
+	return []Paper{p1, p2}
 }
-func (r *PaperRepository) GetPapaers(keyword string) []Paper {
-  p1 := make(Paper)
-  p1["id"] = 1000
-  p1["userId"] = 1000
-  p1["contentId"] = 2000
-  
-  p2 := make(Paper)
-  p2["id"] = 1001
-  p2["userId"] = 1000
-  p2["contentId"] = 2001
-  
-  return []Paper{p1, p2}
+func (r *PaperRepository) GetPapers(keyword string) []Paper {
+	p1 := make(Paper)
+	p1["id"] = 1000
+	p1["userId"] = 1000
+	p1["contentId"] = 2000
+
+	p2 := make(Paper)
+	p2["id"] = 1001
+	p2["userId"] = 1000
+	p2["contentId"] = 2001
+
+	return []Paper{p1, p2}
 }
 
 func (r *PaperRepository) CreatePaper(p Paper) error {
